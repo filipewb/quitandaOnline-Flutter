1df00ee001e23a81ee4a4dbe5081138c4dd919f0
personal <filipe.boeck@hotmail.com>
2023-02-20 10:19:08 -0300

    adicionando os dados do container

--- lib/src/pages/product/product_screen.dart
+++ lib/src/pages/product/product_screen.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:greengrocer/src/config/custom_colors.dart';
 import 'package:greengrocer/src/models/item_model.dart';
+import 'package:greengrocer/src/services/utils_services.dart';
 
 class ProductScreen extends StatelessWidget {
-  const ProductScreen({
+  final ItemModel item;
+  final UtilServices utilServices = UtilServices();
+
+  ProductScreen({
     super.key,
     required this.item,
   });
-
-  final ItemModel item;
 
   @override
   Widget build(BuildContext context) {
@@ -31,6 +34,68 @@ class ProductScreen extends StatelessWidget {
                     offset: const Offset(0, 2),
                   ),
                 ],
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(32),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    // Nome - Quantidade
+                    Row(
+                      children: [
+                        Expanded(
+                          child: Text(
+                            item.itemName,
+                            maxLines: 2,
+                            overflow: TextOverflow.ellipsis,
+                            style: const TextStyle(
+                              fontSize: 27,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                        Container(
+                          height: 30,
+                          width: 70,
+                          color: Colors.red,
+                        ),
+                      ],
+                    ),
+
+                    // Preco
+                    Text(
+                      utilServices.priceToCurrency(item.price),
+                      style: TextStyle(
+                        fontSize: 23,
+                        fontWeight: FontWeight.bold,
+                        color: CustomColors.customSwatchColor,
+                      ),
+                    ),
+
+                    // Descricao
+                    Expanded(
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(
+                          vertical: 10,
+                        ),
+                        child: SingleChildScrollView(
+                          child: Text(
+                            item.description,
+                            style: const TextStyle(
+                              height: 1.5,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+
+                    // Botao
+                    ElevatedButton(
+                      onPressed: () {},
+                      child: Text('Botao'),
+                    ),
+                  ],
+                ),
               ),
             ),
           ),
