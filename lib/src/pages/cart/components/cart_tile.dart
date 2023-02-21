d0cf397112dc971891be58b549b3bc1313af04c8
personal <filipe.boeck@hotmail.com>
2023-02-21 14:33:50 -0300

    criando tiles dos produtos

---
+++ lib/src/pages/cart/components/cart_tile.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:greengrocer/src/config/custom_colors.dart';
+import 'package:greengrocer/src/models/cart_item_model.dart';
+import 'package:greengrocer/src/pages/common_widgets/quantity_widget.dart';
+import 'package:greengrocer/src/services/utils_services.dart';
+
+class CartTile extends StatelessWidget {
+  final CartItemModel cartItem;
+  CartTile({
+    super.key,
+    required this.cartItem,
+  });
+
+  final UtilServices utilServices = UtilServices();
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: const EdgeInsets.fromLTRB(10, 10, 10, 0),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(16),
+      ),
+      child: ListTile(
+        // imagem
+        leading: Image.asset(
+          cartItem.item.imgUrl,
+          height: 60,
+          width: 60,
+        ),
+
+        // titulo
+        title: Text(
+          cartItem.item.itemName,
+          style: const TextStyle(
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+
+        // total
+        subtitle: Text(
+          utilServices.priceToCurrency(cartItem.totalPrice()),
+          style: TextStyle(
+            color: CustomColors.customSwatchColor,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+
+        // quantidade
+        trailing: QuantityWidget(
+          suffixText: cartItem.item.unit,
+          value: cartItem.quantity,
+          result: (quantity) {},
+        ),
+      ),
+    );
+  }
+}
