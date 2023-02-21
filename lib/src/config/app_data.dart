105afc7e6b3099dda881d8c5c35ed85c1305b363
personal <filipe.boeck@hotmail.com>
2023-02-21 15:14:11 -0300

    criando tela de formulario

--- lib/src/config/app_data.dart
+++ lib/src/config/app_data.dart
@@ -1,5 +1,6 @@
 import 'package:greengrocer/src/models/cart_item_model.dart';
 import 'package:greengrocer/src/models/item_model.dart';
+import 'package:greengrocer/src/models/user_model.dart';
 
 ItemModel apple = ItemModel(
   description:
@@ -87,3 +88,11 @@ List<CartItemModel> cartItems = [
     quantity: 3,
   ),
 ];
+
+UserModel user = UserModel(
+  name: 'Filipe',
+  email: 'filipewolfboeck@gmail.com',
+  celular: '99 9 9999-9999',
+  cpf: '999.999.999-99',
+  senha: '',
+);
