cab44351607d3cf46339f9b7966289bcb6bda2bc
personal <filipe.boeck@hotmail.com>
2023-02-25 10:42:41 -0300

    metodo de cadastro

--- lib/src/pages/auth/repository/auth_repository.dart
+++ lib/src/pages/auth/repository/auth_repository.dart
@@ -8,6 +8,15 @@ import 'package:greengrocer/src/services/http_manager.dart';
 class AuthRepository {
   final HttpManager _httpManager = HttpManager();
 
+  AuthResult handleUserOrError(Map<dynamic, dynamic> result) {
+    if (result['result'] != null) {
+      final user = UserModel.fromJson(result['result']);
+      return AuthResult.success(user);
+    } else {
+      return AuthResult.error(authErrors.authErrorsString(result['error']));
+    }
+  }
+
   Future<AuthResult> validateToken(String token) async {
     final result = await _httpManager.restRequest(
       url: EndPoint.validateToken,
@@ -16,13 +25,7 @@ class AuthRepository {
         'X-Parse-Session-Token': token,
       },
     );
-
-    if (result['result'] != null) {
-      final user = UserModel.fromJson(result['result']);
-      return AuthResult.success(user);
-    } else {
-      return AuthResult.error(authErrors.authErrorsString(result['error']));
-    }
+    return handleUserOrError(result);
   }
 
   Future<AuthResult> signIn(
@@ -36,11 +39,16 @@ class AuthRepository {
       },
     );
 
-    if (result['result'] != null) {
-      final user = UserModel.fromJson(result['result']);
-      return AuthResult.success(user);
-    } else {
-      return AuthResult.error(authErrors.authErrorsString(result['error']));
-    }
+    return handleUserOrError(result);
+  }
+
+  Future<AuthResult> signUp(UserModel user) async {
+    final result = await _httpManager.restRequest(
+      url: EndPoint.signup,
+      method: HttpMethod.post,
+      body: user.toJson(),
+    );
+
+    return handleUserOrError(result);
   }
 }
