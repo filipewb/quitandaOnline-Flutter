216b4b5e8ceb35b444b0437e30439957acb7a508
personal <filipe.boeck@hotmail.com>
2023-02-25 11:37:00 -0300

    criando binding

--- lib/src/pages/home/home_tab.dart
+++ lib/src/pages/home/home_tab.dart
@@ -1,8 +1,10 @@
 import 'package:add_to_cart_animation/add_to_cart_animation.dart';
 import 'package:add_to_cart_animation/add_to_cart_icon.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:greengrocer/src/config/custom_colors.dart';
 import 'package:greengrocer/src/pages/common_widgets/custom_shimmer.dart';
+import 'package:greengrocer/src/pages/home/controller/home_controller.dart';
 import 'package:greengrocer/src/services/utils_services.dart';
 
 import '../common_widgets/app_name_widget.dart';
@@ -35,6 +37,9 @@ class _HomeTabState extends State<HomeTab> {
   @override
   void initState() {
     super.initState();
+
+    Get.find<HomeController>().printExample();
+
     Future.delayed(
       const Duration(seconds: 3),
       () {
